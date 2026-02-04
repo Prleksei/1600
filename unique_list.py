33ca224fe59513c495caf2c0973dfb5d36fa970a
makaron00 <mikemakarov666@gmail.com>
2026-02-04 19:41:47 +0100

    hello aq

--- unique_list.py
+++ unique_list.py
@@ -3,3 +3,5 @@ def unique_list(start_list):
     for item in start_list:
         if item not in result:
             result.append(item) 
+test_list = [1, 2, 3, 4, 5, 1, 2, 3]
+print(unique_list(test_list))
